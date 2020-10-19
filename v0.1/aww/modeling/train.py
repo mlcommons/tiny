010f5d68cfd89045314e71cab340db4af2e9e363
Jeremy Holleman <jeremy@syntiant.com>
2020-10-19 04:50:26 -0700

    added early draft of audio wakeword modeling code

---
+++ v0.1/aww/modeling/train.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from tensorflow.lite.experimental.microfrontend.python.ops import audio_microfrontend_op as frontend_op
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import argparse
+
+import models, aww_data
+
+num_classes = 12 # should probably draw this directly from the dataset.
+FLAGS = None
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--data_url',
+      type=str,
+      # pylint: disable=line-too-long
+      default='http://download.tensorflow.org/data/speech_commands_v0.02.tar.gz',
+      # pylint: enable=line-too-long
+      help='Location of speech training data archive on the web.')
+  parser.add_argument(
+      '--data_dir',
+      type=str,
+      default=os.getenv('HOME')+'/data/',
+      help="""\
+      Where to download the speech training data to.
+      """)
+  parser.add_argument(
+      '--background_volume',
+      type=float,
+      default=0.1,
+      help="""\
+      How loud the background noise should be, between 0 and 1.
+      """)
+  parser.add_argument(
+      '--background_frequency',
+      type=float,
+      default=0.8,
+      help="""\
+      How many of the training samples have background noise mixed in.
+      """)
+  parser.add_argument(
+      '--silence_percentage',
+      type=float,
+      default=10.0,
+      help="""\
+      How much of the training data should be silence.
+      """)
+  parser.add_argument(
+      '--unknown_percentage',
+      type=float,
+      default=10.0,
+      help="""\
+      How much of the training data should be unknown words.
+      """)
+  parser.add_argument(
+      '--time_shift_ms',
+      type=float,
+      default=100.0,
+      help="""\
+      Range to randomly shift the training audio by in time.
+      """)
+
+  parser.add_argument(
+      '--sample_rate',
+      type=int,
+      default=16000,
+      help='Expected sample rate of the wavs',)
+  parser.add_argument(
+      '--clip_duration_ms',
+      type=int,
+      default=1000,
+      help='Expected duration in milliseconds of the wavs',)
+  parser.add_argument(
+      '--window_size_ms',
+      type=float,
+      default=30.0,
+      help='How long each spectrogram timeslice is',)
+  parser.add_argument(
+      '--window_stride_ms',
+      type=float,
+      default=10.0,
+      help='How long each spectrogram timeslice is',)
+  parser.add_argument(
+      '--dct_coefficient_count',
+      type=int,
+      default=40,
+      help='How many bins to use for the MFCC fingerprint',)
+  parser.add_argument(
+      '--epochs',
+      type=str,
+      default=100,
+      help='How many epochs to train',)
+  parser.add_argument(
+      '--batch_size',
+      type=int,
+      default=100,
+      help='How many items to train with at once',)
+  parser.add_argument(
+      '--model_architecture',
+      type=str,
+      default='fc4',
+      help='What model architecture to use')
+
+
+  FLAGS, unparsed = parser.parse_known_args()
+
+  print('We will download data to {:}'.format(FLAGS.data_dir))
+  print('we will train for {:d} epochs'.format(FLAGS.epochs))
+  ds_train, ds_test, ds_val = aww_data.get_training_data(FLAGS)
+  print("Done getting data")
+  model = models.get_model(model_name=FLAGS.model_architecture)
+  model.fit(ds_train, validation_data=ds_val, epochs=FLAGS.epochs)
+
