6c4768008e517a9e1f2b929288fa528cdb0620b6
Nat Jeffries <njeff@google.com>
2020-11-11 12:33:10 -0800

    Update models and fix download script.

--- v0.1/reference_submissions/person_detection/download_coco_and_train_vww.sh
+++ v0.1/reference_submissions/person_detection/download_coco_and_train_vww.sh
@@ -7,5 +7,5 @@ unzip train2017.zip
 
 # Preprocess the dataset and train/convert the VWW model.
 python3 parse_coco.py annotations/instances_train2017.json
-python3 train_vww_96.py
+python3 train_vww.py
 python3 convert_vww.py vww_96.h5
