1b2b3554ddddf508539c65f830e47fb263a247d5
Nat Jeffries <natmjeffries@gmail.com>
2021-01-11 06:04:12 -0800

    Update person detection model reference submission.
    Use new API to implement a person detection reference submission for mbed using
    tensorflow lite for microcontrollers.

---
+++ v0.1/reference_submissions/person_detection/setup_example.sh
@@ -0,0 +1,19 @@
+#cd $(dirname $0)
+TFMICRO_DIR=tensorflow
+if [ ! -f "$TFMICRO_DIR" ]; then
+  wget https://github.com/tensorflow/tensorflow/archive/master.zip
+  unzip -o master.zip
+  pushd tensorflow-master
+  make -f tensorflow/lite/micro/tools/make/Makefile generate_microlite_hello_world_project -j18
+  mkdir /tmp/mbed_tmp
+  mv tensorflow/lite/micro/tools/make/gen/linux_x86_64/prj/hello_world/mbed/* /tmp/mbed_tmp
+  popd
+  mv /tmp/mbed_tmp/* .
+  rm tensorflow/lite/micro/examples/hello_world -r 
+fi
+
+mbed-tools new .
+cp ../../api . -r
+cp ../../main.cpp .
+cp ../../util . -r
+cp ../../vww . -r
