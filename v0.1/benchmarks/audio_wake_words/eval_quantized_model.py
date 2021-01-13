fixed scaling on eval_quantize to use scale and zero_point from tflm input_details

v0.1/benchmarks/audio_wake_words/eval_quantized_model.py
@@ -30,7 +30,7 @@ def predict(interpreter, data):
 if __name__ == '__main__':
   Flags, unparsed = aww_util.parse_command()
 
-  num_samps = 2000
+  num_samps = 100
   scale = 0.66
 
   ds_train, ds_test, ds_val = aww_data.get_training_data(Flags)
@@ -45,8 +45,12 @@ if __name__ == '__main__':
   labels = []
   
   test_data = ds_test.unbatch().batch(1).take(num_samps).as_numpy_iterator()
+  input_scale, input_zero_point = input_details[0]["quantization"]
+  print(f"input_scale = {input_scale}, zero point = {input_zero_point}")
+  
   for dat, label in test_data:
-    dat_q = np.array((dat/scale)*256-128, dtype=np.int8)
+    # dat_q = np.array((dat/scale)*256-128, dtype=np.int8)
+    dat_q = np.array(dat/input_scale + input_zero_point, dtype=np.uint8)
     # dat_q = np.expand_dims(dat_q, 3)
     
     interpreter.set_tensor(input_details[0]['index'], dat_q)

v0.1/benchmarks/audio_wake_words/quantize.py
@@ -9,11 +9,11 @@ import aww_util
 if __name__ == '__main__':
   Flags, unparsed = aww_util.parse_command()
 
-  num_calibration_steps = 10
+  num_calibration_steps = 100
   converter = tf.lite.TFLiteConverter.from_saved_model(Flags.saved_model_path)
   converter.optimizations = [tf.lite.Optimize.DEFAULT]
   
-  _, _, ds_val = aww_data.get_training_data(Flags)
+  _, ds_test, ds_val = aww_data.get_training_data(Flags)
   # ds_val = ds_val.batch(1) # can we use a larger batch?
 
   def representative_dataset_gen():
@@ -25,7 +25,7 @@ if __name__ == '__main__':
   converter.optimizations = [tf.lite.Optimize.DEFAULT]
   converter.representative_dataset = representative_dataset_gen
   converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-  converter.inference_input_type = tf.int8  # or tf.uint8
+  converter.inference_input_type = tf.uint8  # or tf.uint8
   converter.inference_output_type = tf.int8  # or tf.uint8
   tflite_quant_model = converter.convert()
 