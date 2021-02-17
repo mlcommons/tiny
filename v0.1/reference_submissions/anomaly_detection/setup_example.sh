1eb4972e1314c5b67bb2707bee962e185815a69d
Csaba Kiraly <cskiraly@users.noreply.github.com>
2021-02-17 19:42:30 +0100

    Anomaly detection c rebase (#29)
    * anamaly detection based on person detection example
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * fix setup script
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * fix mbed command in setup script
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * parametrize model input/output type
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * update OpResolver ops
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * use anomaly detection model and parameters
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * invoke: loop over histogram and call invoke for each window
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * adding test data
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * fix input size while getting histogram from framework
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * MicroMutableOpResolver should be static
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * removing unused g_model_len
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * introduce kSpectrogramSliceCount
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * submitter_implemented.h should be in submitter folder
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * fixes in setup_example.sh
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * adding some debug printing
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * run a warmup inference in th_final_initialize
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * fixup: setup_example
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * reducing MicroMutableOpResolver size to 3
    
    This seems to fix a misterious crash. To be verified.
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * enabling float printf
    
    based on
    https://github.com/ARMmbed/mbed-os/blob/master/platform/source/minimal-printf/README.md
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * input_zero_point is integer
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * add TFLite code in-line instead of MicroModelRunner
    
    Adding code directly based on working example instead of using the
    MicroModelRunner which caused crashes during calls to Invoke.
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * removing all debug printing code
    
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * Add minimal README
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * fix setup script: use config and deploy
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * fix setup clean script
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * Delete download_coco_and_train_vww.sh
    
    This file was copied from the VWW ref submission and is a duplicate
    
    Co-authored-by: Colby Banbury <cbanbury@g.harvard.edu>

---
+++ v0.1/reference_submissions/anomaly_detection/setup_example.sh
@@ -0,0 +1,40 @@
+if [ "$1" == "clean" ]; then
+  rm  api/internally*
+  rm -rf main.cpp
+  rm -rf util
+  rm -rf tensorflow-master.zip
+  rm -rf tensorflow
+  rm -rf mbed-os*
+  rm -rf mbed_settings*
+  rm -rf master*
+  rm -rf tensorflow-master
+  rm -f CMakeLists.txt
+  rm -rf BUILD
+  rm -rf third_party
+  rm -f LICENSE
+  rm -f README_MBED.md
+  rm -rf __pycache__
+
+else
+
+#cd $(dirname $0)
+  TFMICRO_DIR=tensorflow
+  if [ ! -f "$TFMICRO_DIR" ]; then
+    wget https://github.com/tensorflow/tensorflow/archive/master.zip
+    unzip -o master.zip
+    pushd tensorflow-master
+    make -f tensorflow/lite/micro/tools/make/Makefile third_party_downloads
+    make -f tensorflow/lite/micro/tools/make/Makefile generate_hello_world_mbed_project -j18
+    mv tensorflow/lite/micro/tools/make/gen/*/prj/hello_world/mbed/* ../
+    popd
+    rm -rf tensorflow-master
+    rm -rf tensorflow/lite/micro/examples/hello_world
+  fi
+
+  mbed config root .
+  mbed deploy
+  cp ../../api/internally* api/
+  cp ../../main.cpp .
+  cp -r ../../util .
+
+fi
