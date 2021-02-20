3d8082f85b55cb809aebf2616c7bc2343d7d7ed6
Pietro Montino <70531728+PieMonty@users.noreply.github.com>
2021-02-20 17:09:11 +0100

    Bm dev (#30)
    * readme update , vww moved to trained models
    
    * saving y_labels for benchmark samples
    
    * conversion performed using quantization samples
    
    * performance samples loader
    
    * performance samples get loaded by accessing performance samples indexes
    
    * tflite models update based on new calibration samples

---
+++ v0.1/training/image_classification/perf_samples_loader.py
@@ -0,0 +1,27 @@
+import numpy as np
+import os
+import train
+from PIL import Image
+
+perf_samples_dir = 'perf_samples'
+cifar_10_dir = 'cifar-10-batches-py'
+
+if __name__ == '__main__':
+    if not os.path.exists(perf_samples_dir):
+        os.makedirs(perf_samples_dir)
+
+    if os.path.exists('y_labels.txt'):
+        os.remove('y_labels.txt')
+    label_output_file = open('y_labels.txt', 'a')
+
+    _idxs = np.load('perf_samples_idxs.npy')
+    train_data, train_filenames, train_labels, test_data, test_filenames, test_labels, label_names = \
+        train.load_cifar_10_data(cifar_10_dir)
+
+    for i in _idxs:
+        _output_str = '{name},{classes},{label}\n'.format(name=test_filenames[i].decode('UTF-8'), classes=10, label=np.argmax(test_labels[i]))
+        label_output_file.write(_output_str)
+        sample_img = np.array(test_data[i])
+        im = Image.fromarray(sample_img)
+        im.save(perf_samples_dir + '/' + test_filenames[i].decode('UTF-8'))
+    label_output_file.close()
