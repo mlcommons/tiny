69bc3874633ca73e4a4aec1fb1338e4e72b26451
piemonty <pito.montino@gmail.com>
2021-02-23 16:14:49 +0100

    saving perf samples in bmp format

--- v0.1/training/image_classification/perf_samples_loader.py
+++ v0.1/training/image_classification/perf_samples_loader.py
@@ -10,9 +10,7 @@ if __name__ == '__main__':
     if not os.path.exists(perf_samples_dir):
         os.makedirs(perf_samples_dir)
 
-    if os.path.exists('y_labels.txt'):
-        os.remove('y_labels.txt')
-    label_output_file = open('y_labels.txt', 'a')
+    label_output_file = open('y_labels.txt', 'w')
 
     _idxs = np.load('perf_samples_idxs.npy')
     train_data, train_filenames, train_labels, test_data, test_filenames, test_labels, label_names = \
@@ -22,6 +20,6 @@ if __name__ == '__main__':
         _output_str = '{name},{classes},{label}\n'.format(name=test_filenames[i].decode('UTF-8'), classes=10, label=np.argmax(test_labels[i]))
         label_output_file.write(_output_str)
         sample_img = np.array(test_data[i])
-        im = Image.fromarray(sample_img)
-        im.save(perf_samples_dir + '/' + test_filenames[i].decode('UTF-8'))
+        im = Image.fromarray(sample_img, mode='RGB')
+        im.save(perf_samples_dir + '/' + test_filenames[i].decode('UTF-8')[:-3] + 'bmp', quality='keep')
     label_output_file.close()
