20040f534822638d2fdac69ec970c47f2d1ff437
Csaba Kiraly <cskiraly@users.noreply.github.com>
2021-02-23 15:07:15 +0100

    Anomaly detection: model update and adding in8->int8 version (#31)
    * updated model
    
    Updated model trained on both DCASE development and additional
    datasets. This is the exact same model than what is the basis for the
    C++ code.
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * int8->int8 version of the model with removed quantization layers
    
    This is the model used in the C++ code.
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    
    * also download DCASE additional dataset
    
    The training part of both datasets was used for the reference model.
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>

--- v0.1/training/anomaly_detection/get_dataset.sh
+++ v0.1/training/anomaly_detection/get_dataset.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-URL="https://zenodo.org/record/3678171/files/dev_data_ToyCar.zip?download=1"
+URL1="https://zenodo.org/record/3678171/files/dev_data_ToyCar.zip?download=1"
 ZIPFILE="dev_data_ToyCar.zip"
 
-curl $URL -o $ZIPFILE || wget $URL -O $ZIPFILE
+URL2="https://zenodo.org/record/3727685/files/eval_data_train_ToyCar.zip?download=1"
+
 mkdir -p dev_data
+
+curl $URL1 -o $ZIPFILE || wget $URL1 -O $ZIPFILE
 unzip $ZIPFILE -d dev_data
+rm $ZIPFILE
+
+curl $URL2 -o $ZIPFILE || wget $URL2 -O $ZIPFILE
+unzip $ZIPFILE -d dev_data
+rm $ZIPFILE
