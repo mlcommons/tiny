a9fce3ace9ae38372a92eb3f32e93ee16cf1dd93
jeremy-syn <73120277+jeremy-syn@users.noreply.github.com>
2021-03-01 16:07:38 -0500

    Pretrained model plus several code updates (#25)
    * Added requirements.txt file to state pip install packages
    
    * Added plotting function, lr scheduling function
    
    * can test quantized accuracy, but it is terrible (15%).  Re-quantizing filterbank output to INT8 for NN input
    
    * Added parameter to specify bg noise path
    
    * Made changes to input size of the model to be equal to one specified in Hello Edge
    
    * LR schedule as specified in Hello Edge
    
    * Change train.py to pick up the get_data_exp file
    
    * New preprocessing with get_data_expand a command line file to show the command I run
    
    * Exact command as used in HE
    
    * Minor app changes to match HE
    
    * added cl arg for specgram datatype and to force preprocessing (mainly for development)
    
    * added aww_model.tflite to .gitignore
    
    * fixed scaling in eval_quantized_model.py
    
    * moved get_dataset_exp to get_datset; removed unneeded imports from train.py; changed dct count default to 10
    
    * made model settings respect command line arguments
    
    * finished integrating Urmish's mods
    
    * can test quantized accuracy, but it is terrible (15%).  Re-quantizing filterbank output to INT8 for NN input
    
    * added cl arg for specgram datatype and to force preprocessing (mainly for development)
    
    * added aww_model.tflite to .gitignore
    
    * fixed scaling in eval_quantized_model.py
    
    * removed merge artifact
    
    * fixed dim mismatch in eval_quantized_model
    
    * fixed dimension mismatch in quantize
    
    * fixed scaling on eval_quantize to use scale and zero_point from tflm input_details
    
    * removed unused kw args preproc_dtype and force_preproc since they are no longer relevant
    
    * minor clean-up
    
    * moved lr and plot into aww_util; added option to select data(sub)set for eval_quant
    
    * added functions to create C files from an input pattern
    
    * added a (temporary) tflite file
    
    * changed input type in tfl model to int8 from uint8 (changes in quantize.py and eval_quantized_model.py)
    
    * modified get_dataset to generate an input pattern
    
    * added tflite model
    
    * added reference model as saved_model and as tflite file
    
    * removed two scratch/local files
    
    * add saved_model directory; removed redundant tflite file
    
    * added README for aww pretrained models
    
    * merged conflicts and re-added trained_model files
    
    Co-authored-by: Urmish Thakker <urmisht@em-labhostg15.sambanovasystems.com>

--- v0.1/training/audio_wake_words/quantize.py
+++ v0.1/training/audio_wake_words/quantize.py
@@ -9,22 +9,22 @@ import aww_util
 if __name__ == '__main__':
   Flags, unparsed = aww_util.parse_command()
 
-  num_calibration_steps = 10
+  num_calibration_steps = 100
   converter = tf.lite.TFLiteConverter.from_saved_model(Flags.saved_model_path)
   converter.optimizations = [tf.lite.Optimize.DEFAULT]
   
   _, _, ds_val = aww_data.get_training_data(Flags)
-  # ds_val = ds_val.batch(1) # can we use a larger batch?
+  ds_val = ds_val.unbatch().batch(1) 
 
   def representative_dataset_gen():
     for _ in range(num_calibration_steps):
-      next_input = np.expand_dims(next(ds_val.as_numpy_iterator())[0], 3)
+      next_input = next(ds_val.as_numpy_iterator())[0]
       yield [next_input]
     
   converter.optimizations = [tf.lite.Optimize.DEFAULT]
   converter.representative_dataset = representative_dataset_gen
   converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-  converter.inference_input_type = tf.int8  # or tf.uint8
+  converter.inference_input_type = tf.int8  # or tf.uint8; should match dat_q in eval_quantized_model.py
   converter.inference_output_type = tf.int8  # or tf.uint8
   tflite_quant_model = converter.convert()
 
--- v0.1/training/audio_wake_words/train.py
+++ v0.1/training/audio_wake_words/train.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
 
-import tensorflow as tf
-import tensorflow_datasets as tfds
-from tensorflow.lite.experimental.microfrontend.python.ops import audio_microfrontend_op as frontend_op
-from tensorflow import keras
-from tensorflow.keras import layers
-
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import argparse
+from tensorflow import keras
 
 import keras_model as models
 import get_dataset as aww_data
@@ -23,12 +18,22 @@ if __name__ == '__main__':
 
   print('We will download data to {:}'.format(Flags.data_dir))
   print('We will train for {:} epochs'.format(Flags.epochs))
+
   ds_train, ds_test, ds_val = aww_data.get_training_data(Flags)
   print("Done getting data")
-  model = models.get_model(model_name=Flags.model_architecture)
-  model.summary()
+
+  if Flags.model_init_path is None:
+    print("Starting with untrained model")
+    model = models.get_model(args=Flags)
+  else:
+    print(f"Starting with pre-trained model from {Flags.model_init_path}")
+    model = keras.models.load_model(Flags.model_init_path)
+
+    model.summary()
   
-  train_hist = model.fit(ds_train, validation_data=ds_val, epochs=Flags.epochs)
+  callbacks = aww_util.get_callbacks(args=Flags)
+  train_hist = model.fit(ds_train, validation_data=ds_val, epochs=Flags.epochs, callbacks=callbacks)
+  aww_util.plot_training(Flags.plot_dir,train_hist)
   model.save(Flags.saved_model_path)
   
   if Flags.run_test_set:
