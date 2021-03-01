6adbb5dd417a188a4a0435f8dfecdd36284db8e5
Pietro Montino <70531728+PieMonty@users.noreply.github.com>
2021-03-01 04:21:48 +0100

    Bm dev (#33)
    * readme update , vww moved to trained models
    
    * saving y_labels for benchmark samples
    
    * conversion performed using quantization samples
    
    * performance samples loader
    
    * performance samples get loaded by accessing performance samples indexes
    
    * tflite models update based on new calibration samples
    
    * image classification resources
    
    * saving perf samples in bmp format
    
    * loading perf images as bin, flattened in fortran style
    
    * loading images with default bit orders
    
    * image classification benchmark resources

--- v0.1/training/image_classification/perf_samples_loader.py
+++ v0.1/training/image_classification/perf_samples_loader.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 import train
-from PIL import Image
+import struct
 
 perf_samples_dir = 'perf_samples'
 cifar_10_dir = 'cifar-10-batches-py'
@@ -10,9 +10,7 @@ if __name__ == '__main__':
     if not os.path.exists(perf_samples_dir):
         os.makedirs(perf_samples_dir)
 
-    if os.path.exists('y_labels.txt'):
-        os.remove('y_labels.txt')
-    label_output_file = open('y_labels.txt', 'a')
+    label_output_file = open('y_labels.txt', 'w')
 
     _idxs = np.load('perf_samples_idxs.npy')
     train_data, train_filenames, train_labels, test_data, test_filenames, test_labels, label_names = \
@@ -21,7 +19,13 @@ if __name__ == '__main__':
     for i in _idxs:
         _output_str = '{name},{classes},{label}\n'.format(name=test_filenames[i].decode('UTF-8'), classes=10, label=np.argmax(test_labels[i]))
         label_output_file.write(_output_str)
-        sample_img = np.array(test_data[i])
-        im = Image.fromarray(sample_img)
-        im.save(perf_samples_dir + '/' + test_filenames[i].decode('UTF-8'))
+        sample_img = np.array(test_data[i]).flatten()
+
+        f = open(perf_samples_dir + '/' + test_filenames[i].decode('UTF-8')[:-3] + 'bin', "wb")
+        mydata = sample_img
+        myfmt = 'B' * len(mydata)
+        bin = struct.pack(myfmt, *mydata)
+        f.write(bin)
+        f.close()
+
     label_output_file.close()
