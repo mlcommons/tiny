56832f2d1d86210e1770f84dd4d7f2f359a98439
Csaba Kiraly <csaba.kiraly@gmail.com>
2021-03-12 16:46:19 +0100

    make sure mbed customizations are not overwritten by TF
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>

--- v0.1/reference_submissions/anomaly_detection/setup_example.sh
+++ v0.1/reference_submissions/anomaly_detection/setup_example.sh
@@ -31,7 +31,7 @@ else
     pushd tensorflow-*	# we can't use TF_VERSION here, as github seems not to be consistent with naming (v2.3.1 vs 2.3.1) 
     make -f tensorflow/lite/micro/tools/make/Makefile TAGS=$TF_MAKE_TAGS third_party_downloads
     make -f tensorflow/lite/micro/tools/make/Makefile TAGS=$TF_MAKE_TAGS generate_hello_world_mbed_project -j18
-    mv tensorflow/lite/micro/tools/make/gen/*/prj/hello_world/mbed/* ../
+    mv -n tensorflow/lite/micro/tools/make/gen/*/prj/hello_world/mbed/* ../
     popd
     rm -rf tensorflow-*
     rm -rf tensorflow/lite/micro/examples/hello_world
