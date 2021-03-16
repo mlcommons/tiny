a29f547ad3c1e0cfd248fdc5d9a2b3f843ab4b13
Nat Jeffries <natmjeffries@gmail.com>
2021-03-16 10:31:40 -0700

    Support CMSIS-NN and float formatting in printf (#44)
    * Add printf to mbed build, include submitter_implemented.h rather than copying it.
    
    * Build with CMSIS-NN

--- v0.1/reference_submissions/person_detection/setup_example_linux.sh
+++ v0.1/reference_submissions/person_detection/setup_example_linux.sh
@@ -1,13 +1,19 @@
+# define TensorFlow version as git branch/tag/hash
+TF_VERSION=v2.3.1
+
+# enable CMSIS-NN
+TF_MAKE_TAGS="cmsis-nn"
+
 if [ "$1" == "clean" ]; then
-  rm -rf api
+  rm -rf api/internally*
   rm -rf main.cpp
   rm -rf util
   rm -rf vww
   rm -rf tensorflow-master.zip
   rm -rf tensorflow
-  rm -rf mbed* 
-  rm -rf master*
-  rm -rf tensorflow-master
+  rm -rf mbed-os 
+  rm -rf $TF_VERSION*
+  rm -rf $TF_VERSION.zip
   rm -f CMakeLists.txt
   rm -rf BUILD
   rm -rf third_party
@@ -20,19 +26,20 @@ else
 #cd $(dirname $0)
   TFMICRO_DIR=tensorflow
   if [ ! -f "$TFMICRO_DIR" ]; then
-    wget https://github.com/tensorflow/tensorflow/archive/master.zip
-    unzip -o master.zip
-    pushd tensorflow-master
-    make -f tensorflow/lite/micro/tools/make/Makefile third_party_downloads
-    make -f tensorflow/lite/micro/tools/make/Makefile generate_hello_world_mbed_project -j18
-    mv tensorflow/lite/micro/tools/make/gen/linux_x86_64_default/prj/hello_world/mbed/* ../
+    wget https://github.com/tensorflow/tensorflow/archive/$TF_VERSION.zip
+    unzip -o $TF_VERSION.zip
+    pushd tensorflow-*
+    make -f tensorflow/lite/micro/tools/make/Makefile TAGS=$TF_MAKE_TAGS third_party_downloads
+    make -f tensorflow/lite/micro/tools/make/Makefile TAGS=$TF_MAKE_TAGS generate_hello_world_mbed_project -j18
+    mv -n tensorflow/lite/micro/tools/make/gen/linux_x86_64/prj/hello_world/mbed/* ../
     popd
-    rm -rf tensorflow-master
+    rm -rf tensorflow-*
     rm -rf tensorflow/lite/micro/examples/hello_world
   fi
 
+  mbed config root .
   mbed deploy .
-  cp -r ../../api .
+  cp -r ../../api/internally* api/
   cp -r ../../main.cpp .
   cp -r ../../util .
   cp -r ../../training/visual_wake_words/trained_models/vww .
