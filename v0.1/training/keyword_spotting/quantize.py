63819f7ac03b49da3b390623f6f28c931320fc7b
Jeremy Holleman <jeremy@syntiant.com>
2021-03-29 02:43:37 -0700

    sequentially skipping through quant calibration elements; works but very slow

--- v0.1/training/keyword_spotting/quantize.py
+++ v0.1/training/keyword_spotting/quantize.py
@@ -15,10 +15,22 @@ if __name__ == '__main__':
   
   _, _, ds_val = kws_data.get_training_data(Flags)
   ds_val = ds_val.unbatch().batch(1) 
+  print("finished loading and unbatching/rebatching dataset")
 
+  with open("quant_cal_idxs.txt") as fpi:
+    cal_indices = [int(line) for line in fpi]
+  cal_indices.sort()
+
+  
   def representative_dataset_gen():
-    for _ in range(num_calibration_steps):
-      next_input = next(ds_val.as_numpy_iterator())[0]
+    current_index = 0
+    for idx in cal_indices:
+      # we want to element idx out of ds_val.  is there a cleaner way? JH
+      print(f"About to find element {idx}")
+      while current_index <= idx:
+        next_input = next(ds_val.as_numpy_iterator())[0]
+        current_index += 1
+      print(f"About to return element {idx}")
       yield [next_input]
     
   converter.optimizations = [tf.lite.Optimize.DEFAULT]
@@ -26,8 +38,10 @@ if __name__ == '__main__':
   converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
   converter.inference_input_type = tf.int8  # or tf.uint8; should match dat_q in eval_quantized_model.py
   converter.inference_output_type = tf.int8  # or tf.uint8
-  tflite_quant_model = converter.convert()
 
+  print("Setup complete.  About to convert model")
+  tflite_quant_model = converter.convert()
+  print("Just converted model")
   with open(Flags.tfl_file_name, "wb") as fpo:
     fpo.write(tflite_quant_model)
 
