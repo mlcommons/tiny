93c683fc1ff9a2c59fdf62e8bdbd066e2077d231
Jeremy Holleman <jeremy@syntiant.com>
2021-03-30 13:13:34 -0700

    this (quantize.py; cal subset code) doesn't reallywork, but I want to capture it anyway

--- v0.1/training/keyword_spotting/quantize.py
+++ v0.1/training/keyword_spotting/quantize.py
@@ -13,28 +13,33 @@ if __name__ == '__main__':
   converter = tf.lite.TFLiteConverter.from_saved_model(Flags.saved_model_path)
   converter.optimizations = [tf.lite.Optimize.DEFAULT]
   
-  _, _, ds_val = kws_data.get_training_data(Flags, val_cal_subset=True)
+  _, _, ds_val = kws_data.get_training_data(Flags) # , val_cal_subset=True)
   ds_val = ds_val.unbatch().batch(1) 
   print("finished loading and unbatching/rebatching dataset")
 
   with open("quant_cal_idxs.txt") as fpi:
     cal_indices = [int(line) for line in fpi]
   cal_indices.sort()
+  cal_indices = np.arange(15)
+  
+  # def representative_dataset_gen():
+  #   for idx in cal_indices:
+  #     next_input = next(ds_val.as_numpy_iterator())[0]
+  #     yield [next_input]
 
   def representative_dataset_gen():
-    for idx in cal_indices:
+    for _ in range(num_calibration_steps):
       next_input = next(ds_val.as_numpy_iterator())[0]
       yield [next_input]
-    
+
+  
   converter.optimizations = [tf.lite.Optimize.DEFAULT]
   converter.representative_dataset = representative_dataset_gen
   converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
   converter.inference_input_type = tf.int8  # or tf.uint8; should match dat_q in eval_quantized_model.py
   converter.inference_output_type = tf.int8  # or tf.uint8
 
-  print("Setup complete.  About to convert model")
   tflite_quant_model = converter.convert()
-  print("Just converted model")
   with open(Flags.tfl_file_name, "wb") as fpo:
     fpo.write(tflite_quant_model)
 
