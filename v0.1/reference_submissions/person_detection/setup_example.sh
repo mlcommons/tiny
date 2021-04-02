5d596ba359015fdd8984daebd98733ed091df813
Nat Jeffries <natmjeffries@gmail.com>
2021-04-02 11:45:29 -0700

    Clean up README, download script and add quantization calibration dataset list. (#56)
    * Add list of files used for quantized calibration.
    
    * Update REAME and setup script.

--- v0.1/reference_submissions/person_detection/setup_example.sh
+++ v0.1/reference_submissions/person_detection/setup_example.sh
@@ -31,7 +31,7 @@ else
     pushd tensorflow-*
     make -f tensorflow/lite/micro/tools/make/Makefile TAGS=$TF_MAKE_TAGS third_party_downloads
     make -f tensorflow/lite/micro/tools/make/Makefile TAGS=$TF_MAKE_TAGS generate_hello_world_mbed_project -j18
-    mv -n tensorflow/lite/micro/tools/make/gen/linux_x86_64/prj/hello_world/mbed/* ../
+    mv -n tensorflow/lite/micro/tools/make/gen/*/prj/hello_world/mbed/* ../
     popd
     rm -rf tensorflow-*
     rm -rf tensorflow/lite/micro/examples/hello_world
