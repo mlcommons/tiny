1ccf60c413d35ae91c94bba27c330a5843cf53a8
petertorelli <peter.j.torelli@gmail.com>
2021-04-16 09:38:19 -0700

    Cleanup unclear issues regarding EE #defs

--- v0.1/api/internally_implemented.h
+++ v0.1/api/internally_implemented.h
@@ -39,7 +39,7 @@ The file name has been changed and some functions removed.
 /* Version 1.0 of the benchmark only supports these models */
 #define EE_MODEL_VERSION_KWS01 "kws01"
 #define EE_MODEL_VERSION_VWW01 "vww01"
-#define EE_MODEL_VERSION_DCASE01 "ad01"
+#define EE_MODEL_VERSION_AD01 "ad01"
 #define EE_MODEL_VERSION_IC01 "ic01"
 
 typedef enum { EE_ARG_CLAIMED, EE_ARG_UNCLAIMED } arg_claimed_t;
