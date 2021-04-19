bbcd70f90c6954a8f88dcaf8d344276bca63a605
Peter Torelli <peter.j.torelli@gmail.com>
2021-04-19 05:53:10 -0700

    cleanup #defines & add comments (#68)
    * Cleanup unclear issues regarding EE #defs
    
    * See long description.
    
    1. Updated 3 of 4 models to properly switch output message
    if EE_CFG_ENERGY_MODE is set/reset.
    2. Added comments to ad01 explaining perf mode baud is 921600
    3. Switched DCASE01 to AD01 in type #def

--- v0.1/reference_submissions/image_classification/api/submitter_implemented.h
+++ v0.1/reference_submissions/image_classification/api/submitter_implemented.h
@@ -37,12 +37,24 @@ methods from th_libc.h and all testharness methods from th_lib.h are here.
 /// \brief These defines set logging prefixes for test harness integration.
 /// \detail This API is designed for performance evaluation only. In order to
 /// gather energy measurments we recommend using the EEMBC test suite.
-#define EE_MSG_TIMESTAMP_MODE "m-timestamp-mode-energy\r\n"
 #define EE_MSG_TIMESTAMP "m-lap-us-%lu\r\n"
 #define TH_VENDOR_NAME_STRING "unspecified"
 
 #define MAX_DB_INPUT_SIZE (32 * 32 * 3)
 #define TH_MODEL_VERSION EE_MODEL_VERSION_IC01
+
+// Use this to switch between DUT-direct (perf) & DUT-inderrect (energy) modes
+#ifndef EE_CFG_ENERGY_MODE
+#define EE_CFG_ENERGY_MODE 0
+#endif
+
+// This is a visual cue to the user when reviewing logs or plugging an
+// unknown device into the system.
+#if EE_CFG_ENERGY_MODE == 1
+#define EE_MSG_TIMESTAMP_MODE "m-timestamp-mode-energy\r\n"
+#else
+#define EE_MSG_TIMESTAMP_MODE "m-timestamp-mode-performance\r\n"
+#endif
 
 #include <stdarg.h>
 #include <stdio.h>
