6180b6299be154551f7860761a388afc1a347116
Jeremy Holleman <jeremy@syntiant.com>
2021-05-19 13:56:49 -0700

    changed quantize to also create fp32 tflite model and changed eval_quant to detect tfl input type and work correctly with int8 or fp32 inputs

--- v0.1/training/keyword_spotting/quantize.py
+++ v0.1/training/keyword_spotting/quantize.py
@@ -14,6 +14,12 @@ if __name__ == '__main__':
   converter = tf.lite.TFLiteConverter.from_keras_model(model)
   converter.optimizations = [tf.lite.Optimize.DEFAULT]
   
+  fp32_tfl_file_name = Flags.tfl_file_name[:Flags.tfl_file_name.rfind('.')] + '_float32.tflite'
+  tflite_float_model = converter.convert()
+  with open(fp32_tfl_file_name, "wb") as fpo:
+    num_bytes_written = fpo.write(tflite_float_model)
+  print(f"Wrote {num_bytes_written} / {len(tflite_float_model)} bytes to tflite file")
+
   with open("quant_cal_idxs.txt") as fpi:
     cal_indices = [int(line) for line in fpi]
   cal_indices.sort()
