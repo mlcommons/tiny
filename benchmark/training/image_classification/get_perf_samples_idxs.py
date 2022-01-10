b3fe2defc9cf537fe999d5ff3393f48f41e85995
Javier M. Duarte <jduarte@ucsd.edu>
2022-01-10 11:24:50 -0800

    update random seed

--- benchmark/training/image_classification/get_perf_samples_idxs.py
+++ benchmark/training/image_classification/get_perf_samples_idxs.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import train
 
+RANDOM_SEED = 8108
 perf_samples_dir = 'perf_samples'
 cifar_10_dir = 'cifar-10-batches-py'
 
@@ -17,7 +18,7 @@ if __name__ == '__main__':
     my_labels = test_labels.argmax(axis=-1)
     all_idxs = np.arange(len(test_labels))
     balanced_idxs = []
-    rng = np.random.default_rng(42)
+    rng = np.random.default_rng(RANDOM_SEED)
     for label in range(10):
         mask = my_labels == label
         class_idxs = rng.choice(all_idxs[mask], size=20, replace=False)
