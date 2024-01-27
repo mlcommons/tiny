03c9d8b9ae42a0cbfcca49f907d1859e50df984d
Jeremy Holleman <jeremy@syntiant.com>
2024-01-27 06:09:44 -0800

    updated streaming wakeword model to be the actual candidate DS-TCN model w/ no residual layers

--- benchmark/training/streaming_wakeword/keras_model.py
+++ benchmark/training/streaming_wakeword/keras_model.py
@@ -57,28 +57,175 @@ def prepare_model_settings(label_count, args):
     'background_volume_range_': 0.1
   }
 
+def conv_block(inputs,
+                 repeat,
+                 kernel_size,
+                 filters,
+                 dilation,
+                 stride,
+                 filter_separable,
+                 residual=False,
+                 padding='same',
+                 dropout=0.0,
+                 activation='relu',
+                 scale=True,
+                 use_one_step=True,
+                 build_streaming=True):
+  """Convolutional block.  Can be optionally residual
+
+  It is based on paper
+  Jasper: An End-to-End Convolutional Neural Acoustic Model
+  https://arxiv.org/pdf/1904.03288.pdf
+
+  Args:
+    inputs: input tensor
+    repeat: number of repeating DepthwiseConv1D and Conv1D block
+    kernel_size: kernel size of DepthwiseConv1D in time dim
+    filters: number of filters in DepthwiseConv1D and Conv1D
+    dilation: dilation in time dim for DepthwiseConv1D
+    stride: stride in time dim for DepthwiseConv1D
+    filter_separable: use separable conv or standard conv
+    residual: if True residual connection is added
+    padding: can be 'same' or 'causal'
+    dropout: dropout value
+    activation: type of activation function (string)
+    scale: apply scaling in batchnormalization layer
+    use_one_step: this parameter will be used for streaming only
+
+  Returns:
+    output tensor
+
+  Raises:
+    ValueError: if padding has invalid value
+  """
+  if residual and (padding not in ('same', 'causal')):
+    raise ValueError('padding should be same or causal for residual blocks')
+
+  net = inputs
+  
+  if kernel_size > 0:
+    print("under kernel_size > 0")
+    # DepthwiseConv1D
+    if build_streaming: # otherwise the Stream() wrapper handles the padding
+      dw_pad = 'valid'
+    else:
+      if padding=='causal':
+        print("Adding padding before DWConv2D")
+        net = tf.pad(net, [[0, 0], [kernel_size-1, 0], [0, 0], [0, 0]], 'constant')
+        dw_pad = 'valid'
+      elif padding == 'valid':
+        dw_pad = 'valid'
+      elif padding == 'same':
+        dw_pad = 'same'
+        
+    net = tf.keras.layers.DepthwiseConv2D(
+        kernel_size=(kernel_size, 1),
+        strides=(stride, stride),
+        padding=dw_pad,
+        dilation_rate=(dilation, 1),
+        use_bias=False)(
+      net)
+
+  # Conv2D 1x1 - streamable by default
+  net = keras.layers.Conv2D(
+      filters=filters, kernel_size=1, use_bias=False, padding='valid')(
+          net)
+  
+  net = keras.layers.BatchNormalization(scale=scale)(net)
+
+  if residual:
+    # Conv1D 1x1 - streamable by default
+    net_res = keras.layers.Conv2D(
+        filters=filters, kernel_size=1, use_bias=False, padding='valid')(
+            inputs)
+    net_res = keras.layers.BatchNormalization(scale=scale)(net_res)
+    print(f"net.shape = {net.shape}, net_res.shape = {net_res.shape}")
+    net = keras.layers.Add()([net, net_res])
+
+  net = keras.layers.Activation(activation)(net)
+  net = keras.layers.Dropout(rate=dropout)(net)
+  return net
+
+
 
 
 def get_model(args):
   model_name = args.model_architecture
 
-  label_count=12
+  label_count=3
   model_settings = prepare_model_settings(label_count, args)
 
 
   if model_name=="dstcn":
     print("DS CNN model invoked")
+
+    ds_filters          = [128, 64, 64, 64, 128]
+    ds_repeat           = [1, 1, 1, 1, 1]
+    ds_residual         = [0, 0, 0, 0, 0]
+    ds_kernel_size      = [5, 5, 11, 13, 15]
+    ds_stride           = [1, 1, 1, 1, 1]
+    ds_dilation         = [1, 1, 1, 1, 1]
+
+    ds_filter_separable = [1, 1, 1, 1, 1]
+    ds_scale            = 1
+    ds_max_pool         = 0
+
+    # check that all the lists are the same length
+    num_blocks = len(ds_filters)
+    for param_list, param_name in [(ds_filters         , "ds_filters"),
+                                   (ds_repeat          , "ds_repeat"),       
+                                   (ds_residual        , "ds_residual"),
+                                   (ds_kernel_size     , "ds_kernel_size"),
+                                   (ds_stride          , "ds_stride"),
+                                   (ds_dilation        , "ds_dilation"),
+                                   (ds_filter_separable, "ds_filter_separable")
+                                   ]:
+      if len(param_list) != num_blocks:
+        raise ValueError(f"All config lists must be the same length ({num_blocks}).  param_name has length {len(param_list)}")
+        
+
     input_shape = [model_settings['spectrogram_length'], # length in time
                    model_settings['dct_coefficient_count'] # number of features
                    ]
     filters = 64
     weight_decay = 1e-4
     regularizer = l2(weight_decay)
-    final_pool_size = (int(input_shape[0]/2), int(input_shape[1]/2))
+
     
     # Model layers
     # Input pure conv2d
     inputs = Input(shape=input_shape)
+
+    net = inputs
+    
+    # make it [batch, time, 1, feature]
+    net = tf.keras.backend.expand_dims(net, axis=2)
+    
+    
+    for count in range(len(ds_stride)):  
+      print(f"count={count}, before resnet block, net shape = {net.shape}")
+      net = conv_block(net, repeat=ds_repeat[count], 
+                         kernel_size=ds_kernel_size[count],
+                         filters=ds_filters[count],
+                         dilation=ds_dilation[count],
+                         stride=ds_stride[count],
+                         filter_separable=ds_filter_separable[count], 
+                         residual=ds_residual[count], 
+                         padding=ds_padding[count], 
+                         scale=flags.ds_scale,
+                         dropout=flags.dropout,
+                         activation=flags.activation, 
+                         use_one_step=(flags.data_stride<=1), 
+                         build_streaming=build_streaming)
+      print(f"after resnet block, net shape = {net.shape}")
+    net = tf.keras.layers.GlobalAveragePooling2D()(net)
+    net = tf.keras.layers.Flatten()(net)
+    net = tf.keras.layers.Dense(flags.label_count)(net) # target + _silence, _noise
+    return tf.keras.Model(input_audio, net)
+    
+    ########################################
+
+    
     x = Flatten()(x)
     outputs = Dense(model_settings['label_count'], activation='softmax')(x)
 
