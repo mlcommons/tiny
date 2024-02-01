2950b1182ef9e6265acdd03fd11184a345ff39e3
Steve Reckamp <sreckamp@gmail.com>
2024-01-31 21:42:28 -0600

    Add raw I2S implementation

---
+++ benchmark/interface/Application/Audio/RawI2SWaveSink.cpp
@@ -0,0 +1,107 @@
+#include "RawI2SWaveSink.hpp"
+
+#include "main.h"
+#include "stm32h5xx_hal_gpio.h"
+#include "stm32h573i_discovery_audio.h"
+#include "tx_semaphore.h"
+
+static INT active_buffer = -1;
+static TX_SEMAPHORE buffer_semaphore;
+//static DMA_HandleTypeDef handle_GPDMA1_Channel7;
+
+//static SAI_HandleTypeDef hsai_BlockA2;
+
+/**
+  * @brief Tx Transfer completed callbacks.
+  * @param  hsai : pointer to a SAI_HandleTypeDef structure that contains
+  *                the configuration information for SAI module.
+  * @retval None
+  */
+//void BSP_AUDIO_OUT_TransferComplete_CallBack(uint32_t instance)
+//{
+//  if(instance == 0)
+//  {
+//    active_buffer = 1;
+//    tx_semaphore_ceiling_put(&buffer_semaphore, 1);
+//  }
+//}
+//
+///**
+//  * @brief Tx Transfer Half completed callbacks
+//  * @param  hsai : pointer to a SAI_HandleTypeDef structure that contains
+//  *                the configuration information for SAI module.
+//  * @retval None
+//  */
+//void BSP_AUDIO_OUT_HalfTransfer_CallBack(uint32_t instance)
+//{
+//  if(instance == 0)
+//  {
+//    active_buffer = 0;
+//    tx_semaphore_ceiling_put(&buffer_semaphore, 1);
+//  }
+//}
+
+namespace Audio
+{
+  RawI2SWaveSink::RawI2SWaveSink(Tasks::TaskRunner &runner, TX_BYTE_POOL &byte_pool)
+        : WaveSink(runner, byte_pool)
+  {
+    HAL_GPIO_WritePin(MEMS_LED_GPIO_Port, MEMS_LED_Pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(DETECTN_GPIO_Port, DETECTN_Pin, GPIO_PIN_SET);
+    if(buffer_semaphore.tx_semaphore_id != TX_SEMAPHORE_ID)
+    {
+      const char *name = "Raw I2S buffer playback semaphore";
+      tx_semaphore_create(&buffer_semaphore, (char *)name, 0);
+    }
+  }
+
+  PlayerState RawI2SWaveSink::GetState()
+  {
+    ULONG state;
+    BSP_AUDIO_OUT_GetState(0, &state);
+    return state == AUDIO_OUT_STATE_RESET
+          ? RESET
+          : (state == AUDIO_OUT_STATE_STOP ? STOPPED : UNKNOWN);
+  }
+
+  PlayerState RawI2SWaveSink::Initialize()
+  {
+    BSP_AUDIO_Init_t init;
+    init.BitsPerSample = AUDIO_RESOLUTION_16B;
+    init.ChannelsNbr = 2;
+    init.Device = AUDIO_OUT_DEVICE_HEADPHONE;
+    init.SampleRate = AUDIO_FREQUENCY_44K;
+    init.Volume = 80;
+
+    BSP_AUDIO_OUT_Init(0, &init);
+    return GetState();
+  }
+
+  void RawI2SWaveSink::Configure(const WaveSource &source)
+  {
+    BSP_AUDIO_OUT_SetBitsPerSample(0, source.GetSampleSize());
+    BSP_AUDIO_OUT_SetChannelsNbr(0, source.GetChannelCount());
+    BSP_AUDIO_OUT_SetSampleRate(0, source.GetFrequency());
+  }
+
+  PlayerResult RawI2SWaveSink::Play(UCHAR *buffer, ULONG size)
+  {
+    return BSP_AUDIO_OUT_Play(0, buffer, size) == BSP_ERROR_NONE ? SUCCESS : ERROR;
+  }
+
+  PlayerResult RawI2SWaveSink::Stop()
+  {
+    return BSP_AUDIO_OUT_Stop(0) == BSP_ERROR_NONE ? SUCCESS : ERROR;
+  }
+
+  INT RawI2SWaveSink::WaitForActiveBuffer()
+  {
+    while(active_buffer == -1) tx_semaphore_get(&buffer_semaphore, 50);
+    return active_buffer;
+  }
+
+  void RawI2SWaveSink::SetActiveBuffer(INT buffer_id)
+  {
+    active_buffer = buffer_id;
+  }
+}
---
+++ benchmark/interface/Application/Audio/RawI2SWaveSink.hpp
@@ -0,0 +1,23 @@
+#ifndef AUDIO_RAWI2SWAVESINK_HPP
+#define AUDIO_RAWI2SWAVESINK_HPP
+
+#include "WaveSink.hpp"
+
+namespace Audio
+{
+  class RawI2SWaveSink : public WaveSink
+  {
+  public:
+    RawI2SWaveSink(Tasks::TaskRunner &runner, TX_BYTE_POOL &byte_pool);
+  protected:
+    void Configure(const WaveSource &source);
+    PlayerState GetState();
+    PlayerState Initialize();
+    PlayerResult Play(UCHAR *buffer, ULONG size);
+    PlayerResult Stop();
+    INT WaitForActiveBuffer();
+    void SetActiveBuffer(INT buffer_id);
+  };
+}
+
+#endif // AUDIO_RAWI2SWAVESINK_HPP
