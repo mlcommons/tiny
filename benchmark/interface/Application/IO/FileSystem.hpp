4f0e73bda700ef53d23d29c229be88c1b710cbfb
Steve Reckamp <sreckamp@gmail.com>
2024-09-16 17:38:02 -0500

    FileSystem comments

--- benchmark/interface/Application/IO/FileSystem.hpp
+++ benchmark/interface/Application/IO/FileSystem.hpp
@@ -9,22 +9,47 @@
 namespace IO
 {
   class ListDirectoryTask;
-  class OpenFileTask;
 
+  /**
+   * File System Abstraction
+   */
   class FileSystem
   {
   public:
+    /**
+     * Constructor
+     * @param runner TaskRunner that executes the submitted Task::ITasks
+     */
     FileSystem(Tasks::TaskRunner &runner);
+
+    /**
+     * Connect the FileSystem to an FX_MEDIA instance
+     * @param media the FX_MEDIA instance in it
+     */
     void SetMedia(FX_MEDIA *media);
+
+    /**
+     * Submit a Task::ITask to list the contents of the directory
+     * @param directory Directory path to list the contents of
+     * @param queue The queue to send the response to
+     * @param show_directory Show directories in the output (default TX_FALSE)
+     * @param show_hidden Show hidden files in the output (default TX_FALSE)
+     */
     void ListDirectory(const std::string &directory, TX_QUEUE *queue = (TX_QUEUE *) TX_NULL, bool show_directory = TX_FALSE,
                        bool show_hidden = TX_FALSE);
+
+    /**
+     * Open a file
+     *
+     * @param file_name The name of the file. If filename is empty. it plays back the memory at 0x08080000
+     * @return The file as an ::IDataSource
+     */
     IDataSource *OpenFile(const std::string &file_name);
   private:
     Tasks::TaskRunner &runner;
     FX_MEDIA *media;
     void IndirectListDirectory(const std::string &directory, TX_QUEUE *queue, bool show_directory, bool show_hidden);
     friend class ListDirectoryTask;
-    friend class OpenFileTask;
   };
 }
 
