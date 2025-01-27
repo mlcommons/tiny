a054d3b4a2c1f07c74085f37af440f381986b753
Jeremy Holleman <jeremy@syntiant.com>
2025-01-27 09:33:26 -0500

    merged tims work on the runner

--- benchmark/runner/datasets.py
+++ benchmark/runner/datasets.py
@@ -4,24 +4,35 @@ from random import randrange
 
 
 class DataSet:
-  def __init__(self, dataset_path, truth_file):
+  def __init__(self, dataset_path, truth_file, start_index=0):
     self._dataset_path = dataset_path
     self._truth_file = truth_file
     self._truth_data = None
+    self._current_index = start_index
 
   def _read_truth_file(self):
     if not self._truth_data:
       with open(os.path.join(self._dataset_path, self._truth_file)) as file:
-        reader = csv.DictReader(file, fieldnames=["file", "classes", "class"])
+        reader = csv.DictReader(file, fieldnames=["file", "classes", "class", "bytes_to_send","stride"])
         self._truth_data = [f for f in reader]
 
   def get_file_by_index(self, index):
     self._read_truth_file()
     data = []
-    index = index if index is not None else randrange(len(self._truth_data))
+    # if an index is not specified, use the object's _current_index and increment it,
+    # if one is specified, use that and don't increment _current_index
+    if index is not None:
+      inc_index = False
+    else:
+      index = self._current_index
+      inc_index = True
+
     truth = None
     if index < len(self._truth_data):
       truth = self._truth_data[index]
       with open(os.path.join(self._dataset_path, truth.get("file")), "rb") as file:
         data = file.read()
+    if inc_index:
+      self._current_index += 1
+
     return truth, data
