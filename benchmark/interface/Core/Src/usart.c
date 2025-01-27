a53724bcd74b83c2c0ea833c9223d76f864c0701
Jeremy Holleman <jeremy@syntiant.com>
2025-01-27 10:25:56 -0500

    removed comment in auto-gen usart.c code

--- benchmark/interface/Core/Src/usart.c
+++ benchmark/interface/Core/Src/usart.c
@@ -42,7 +42,7 @@ void MX_USART1_UART_Init(void)
 
   /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
-  huart1.Init.BaudRate = 115200; // 921600;
+  huart1.Init.BaudRate = 115200;
   huart1.Init.BaudRate = 115200;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
   huart1.Init.StopBits = UART_STOPBITS_1;
