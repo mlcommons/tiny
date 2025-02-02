e01c2d55eb3475f9d3f174cb6dd12454bf599786
jeremy-syn <jeremy@syntiant.com>
2025-02-02 15:25:11 -0500

    added sww_ref_l4r5zi; currently pretty similar to sww_testing

---
+++ benchmark/reference_submissions/streaming_wakeword/sww_ref_l4r5zi/Core/Inc/sww_ref_util.h
@@ -0,0 +1,58 @@
+/*
+ * sww_util.h
+ *
+ *  Created on: Jan 16, 2025
+ *      Author: jeremy
+ */
+
+#ifndef INC_SWW_UTIL_H_
+#define INC_SWW_UTIL_H_
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include "sww_model.h"
+
+#define EE_CMD_SIZE 80u
+#define EE_CMD_DELIMITER " "
+#define EE_CMD_TERMINATOR '%'
+
+#define SWW_WINLEN_SAMPLES 1024
+#define SWW_WINSTRIDE_SAMPLES 512
+#define SWW_MODEL_INPUT_SIZE 1200
+
+// struct for a log we can write to without the delay of printing to UART
+#define LOG_BUFFER_SIZE 4096
+typedef struct {
+    char buffer[LOG_BUFFER_SIZE];
+    size_t current_pos;
+} LogBuffer;
+
+
+
+typedef enum {
+	Idle,
+	FileCapture, // capture a block of a pre-determined size, then stop
+	Streaming,   // continually capture blocks of "window_step" size, then
+	             // extract features and run NN
+	Stopping     // command received to stop streaming detection, but not processed yet
+
+} i2s_state_t;
+
+
+void print_vals_int16(const int16_t *buffer, uint32_t num_vals);
+void print_bytes(const uint8_t *buffer, uint32_t num_bytes);
+void print_vals_float(const float *buffer, uint32_t num_vals);
+void log_printf(LogBuffer *log, const char *format, ...);
+
+void process_command(char *full_command);
+void ee_serial_callback(char c);
+
+ai_error aiInit(void);
+void setup_i2s_buffers();
+void compute_lfbe_f32(const int16_t *pSrc, float32_t *pDst, float32_t *pTmp);
+
+
+#endif /* INC_SWW_UTIL_H_ */
