9e0bba9875100acf111cad57ee42ca7f3307e0eb
jeremy-syn <jeremy@syntiant.com>
2025-05-09 09:01:12 -0400

    changed evaluate.py so it will choose TFLite intepreter or standard model code based on filename

--- benchmark/training/streaming_wakeword/evaluate.py
+++ benchmark/training/streaming_wakeword/evaluate.py
@@ -15,11 +15,8 @@ import get_dataset
 
 Flags = util.parse_command("evaluate")
 
-if not Flags.use_tflite_model and Flags.saved_model_path is None:
-    err_str = "Unless use_tflite_model is specified, saved_model_path is required."
-    raise RuntimeError(err_str)
-if Flags.use_tflite_model and Flags.tfl_file_name is None:  
-    err_str = "When use_tflite_model is specified, tfl_file_name is required."
+if Flags.saved_model_path is None:
+    err_str = "Model to be evaluated must be specified with --saved_model_path"
     raise RuntimeError(err_str)
 
 det_thresh = 0.95
@@ -36,8 +33,10 @@ else:
     wav_file = os.path.join(Flags.wav_dir, wav_file)
     ww_windows = stream_test_config[0]['detection_windows']
 
-if Flags.use_tflite_model:
-    interpreter = tf.lite.Interpreter(model_path=Flags.tfl_file_name)
+use_tflite = Flags.saved_model_path.rsplit('.')[-1] in ["tflite", "tfl"]
+
+if use_tflite:
+    interpreter = tf.lite.Interpreter(model_path=Flags.saved_model_path)
     interpreter.allocate_tensors()
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
@@ -46,16 +45,17 @@ if Flags.use_tflite_model:
     labels = []
     input_scale, input_zero_point = input_details[0]["quantization"]
     output_scale, output_zero_point = output_details[0]["quantization"]
-else:
+elif Flags.saved_model_path.rsplit('.')[-1] in ["h5", "keras"]:
     with tfmot.quantization.keras.quantize_scope(): # needed for the QAT wrappers
         model_std = keras.models.load_model(Flags.saved_model_path) # normal model for fixed-length inputs
-
     ## Build a model that can accept variable-length inputs to process the long waveform/spectrogram
     Flags.variable_length=True
     model_varlen = models.get_model(args=Flags, use_qat=False) # model with variable-length input
     Flags.variable_length=False
     # transfer weights from trained model into variable-length model
     model_varlen.set_weights(model_std.get_weights())
+else:
+    raise RuntimeError("Model file ({Flags.saved_model_path}) must end in '.tflite', '.h5', or '.keras'.")
 
 wav_sampling_freq, long_wav = wavfile.read(wav_file)
 assert wav_sampling_freq == samp_freq
@@ -72,7 +72,7 @@ feature_extractor = get_dataset.get_lfbe_func(data_config)
 long_spec = feature_extractor(long_wav).numpy()
 print(f"Long waveform shape = {long_wav.shape}, spectrogram shape = {long_spec.shape}")
 
-if Flags.use_tflite_model:
+if use_tflite:
     yy_q = np.nan*np.zeros((long_spec.shape[0]-input_shape[1]+1,3))
 
     for idx in range(long_spec.shape[0]-input_shape[1]+1):
@@ -108,7 +108,7 @@ data_dir = Flags.speech_commands_path
 _, _, val_files = get_dataset.get_file_lists(data_dir)
 ds_val = get_dataset.get_data(flags_validation, val_files)
 
-if not Flags.use_tflite_model:
+if not use_tflite:
     val_loss, val_acc, val_prec, val_recl = model_std.evaluate(ds_val)
 
 print(f"Results: false_detections={np.sum(ww_false_detects!=0)},",
@@ -116,7 +116,7 @@ print(f"Results: false_detections={np.sum(ww_false_detects!=0)},",
       f"false_rejections={np.sum(ww_false_rejects!=0)},", end=""
       )
 
-if not Flags.use_tflite_model:
+if not use_tflite:
     print(f"val_loss={val_loss:5.4f}, val_acc={val_acc:5.4f}, val_precision={val_prec:5.4f}, val_recall={val_recl:5.4f}")
 else:
     print("") # We need a newline
