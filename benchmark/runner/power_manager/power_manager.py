1b1d6d248d7c5ee7120f5d52ff2de7bda6f3ea37
TimotheeeNiven <rniven1@uncc.edu>
2025-06-20 16:32:36 -0400

    JS220 Partially Working

--- benchmark/runner/power_manager/power_manager.py
+++ benchmark/runner/power_manager/power_manager.py
@@ -1,13 +1,73 @@
 # power_manager.py
 
-from power_manager.power_manager_lpm import LPMManager
-from power_manager.power_manager_js220 import JoulescopePowerManager
+import re
+import sys
+from queue import Queue
+from threading import Thread
+from serial_device import SerialDevice
+from .power_manager_lpm import LPMCommands
+from .power_manager_js220 import JoulescopeCommands
+
 
 class PowerManager:
-    def __new__(cls, name, **kwargs):
-        if name == 'lpm01a':
-            return LPMManager(**kwargs)
-        elif name == 'js220':
-            return JoulescopePowerManager(**kwargs)
+    PROMPT = "PowerShield > "
+
+    def __init__(self, device_type, port_device=None, baud_rate=None, voltage=3.3, echo=False, js_device=None):
+        self._device_type = device_type
+        self._voltage = voltage
+        self._board_id = None
+        self._version = None
+        self._lcd = [None, None]
+        self._in_prograss = False
+        self._data_queue = Queue()
+        self._message_queue = Queue()
+        self._read_thread = None
+        self._running = False
+
+        if device_type == "lpm01a":
+            self._port = SerialDevice(port_device, baud_rate, "ack|error", "\r\n", echo=echo)
+            self._commands = LPMCommands(self, self._port)
+        elif device_type == "js220":
+            if js_device is None:
+                from joulescope import scan_require_one
+                js_device = scan_require_one(config="ignore")
+            self._commands = JoulescopeCommands(self, js_device=js_device)
+            self._port = self._commands.get_port()
         else:
-            raise ValueError(f"Unsupported power device: {name}")
+            raise ValueError(f"Unsupported device type: {device_type}")
+
+        self.__enter__()
+
+    def __enter__(self):
+        self._port.__enter__()
+        self._commands.setup()
+        self._start_read_thread()
+        return self
+
+    def __exit__(self, *args):
+        self._commands.tear_down()
+        self._stop_read_thread()
+        self._port.__exit__(*args)
+
+    def _start_read_thread(self):
+        self._running = True
+        self._read_thread = Thread(target=self._commands.read_loop)
+        self._read_thread.start()
+
+    def _stop_read_thread(self):
+        self._running = False
+        self._read_thread.join()
+
+    def get_results(self):
+        while not self._data_queue.empty():
+            yield self._data_queue.get()
+
+    # Pass-throughs
+    def power_on(self): return self._commands.power_on()
+    def power_off(self): return self._commands.power_off()
+    def start(self): return self._commands.start()
+    def stop(self): return self._commands.stop()
+    def get_status(self): return self._commands.get_status()
+    def get_board_id(self): return self._commands.get_board_id()
+    def get_version(self): return self._commands.get_version()
+    def set_lcd(self, *args): return self._commands.set_lcd(*args)
--- benchmark/runner/power_manager/power_manager_js220.py
+++ benchmark/runner/power_manager/power_manager_js220.py
@@ -1,27 +1,327 @@
-# power_manager_js220.py
+import time
+from joulescope import scan
 
-from joulescope import scan_require_one
 
-class JoulescopePowerManager:
-    def __init__(self, **kwargs):
-        self.js = scan_require_one()
-        self.js.open()
-        self._data_queue = []
+class JS220PortWrapper:
+    def __init__(self, device):
+        self._device = device
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        return self
+
+    def close(self):
+        return self._device.close()
+
+    def write_line(self, line):
+        pass
+
+    def read_line(self, timeout=None):
+        return None
+
+
+class JoulescopeCommands:
+    def __init__(self, manager, js_device):
+        self.m = manager
+        self._device = js_device
+
+        # ✅ Configure and start streaming
+        self._device.parameter_set("source", "raw")
+        self._device.parameter_set("sensor_power", "on")
+        self._device.parameter_set("sampling_frequency", 1000)
+        self._device.parameter_set("trigger_source", "gpi0")
+        try:
+            self._device.start()
+        except Exception as e:
+            print(f"[JS220] Failed to start device: {e}")
+
+    def get_port(self):
+        return JS220PortWrapper(self._device)
+
+    def setup(self):
+        pass  # Setup handled in __init__
+
+    def tear_down(self):
+        print("[JS220] Shutting down stream...")
+        self.m._running = False
+        try:
+            self._device.stop()
+        except Exception:
+            pass
+        try:
+            self._device.close()
+        except Exception:
+            pass
+        print("[JS220] Shutdown complete.")
+
+    def read_loop(self):
+        sb = self._device.stream_buffer
+        last_sample_id = None
+
+        print("[JS220] Stream reading started (raw)...")
+        while self.m._running:
+            sample_id_range = sb.sample_id_range
+            if sample_id_range is None:
+                time.sleep(0.001)
+                continue
+
+            start_id, end_id = sample_id_range
+            if last_sample_id is None:
+                last_sample_id = start_id
+
+            if end_id <= last_sample_id:
+                time.sleep(0.001)
+                continue
+
+            if end_id < last_sample_id:
+                last_sample_id = end_id
+                continue
+
+            try:
+                data = sb.samples_get(last_sample_id, end_id, fields=["current", "voltage"])
+            except ValueError:
+                last_sample_id = None
+                continue
+
+            t0 = time.time()
+            current = data["signals"]["current"]["value"]
+            voltage = data["signals"]["voltage"]["value"]
+            count = min(len(current), len(voltage))
+            last_sample_id = end_id
+
+            for i in range(count):
+                self.m._data_queue.put([t0, current[i], voltage[i]])
+
+        print("[JS220] Stream reading loop exited.")
 
     def power_on(self):
-        self.js.parameters['sensor_power'] = 'on'
+        return True
 
     def power_off(self):
-        self.js.parameters['sensor_power'] = 'off'
+        return True
 
     def start(self):
-        self.js.stream_configure()
-        self.js.stream_start()
+        return True
 
     def stop(self):
-        self.js.stream_stop()
+        return True
 
-    def get_results(self):
-        for _, data in self.js.data_reader().items():
-            self._data_queue.append(data['energy']['value'])
-        return self._data_queue
+    def get_board_id(self):
+        return self._device.device_path()
+
+    def get_version(self):
+        try:
+            return self._device.info().get("fw")
+        except Exception:
+            return None
+
+    def get_status(self):
+        return None
+
+    def set_lcd(self, *args):
+        return [None, None]
+
+
+"""
+################################################################################
+#                               Joulescope Commands                            #
+################################################################################
+# Parameter: sensor_power
+#   Options:
+#     [0] ('off', 0, [])
+#     [1] ('on', 1, [])
+
+# Parameter: source
+#   Options:
+#     [0] ('off', 0, [])
+#     [1] ('raw', 192, ['on'])
+#     [2] ('pattern_usb', 9, [])
+#     [3] ('pattern_control', 10, [])
+#     [4] ('pattern_sensor', 175, [])
+
+# Parameter: i_range
+#   Options:
+#     [0] ('auto', 128, ['on'])
+#     [1] ('10 A', 1, ['0', 0])
+#     [2] ('2 A', 2, ['1', 1])
+#     [3] ('180 mA', 4, ['2', 2])
+#     [4] ('18 mA', 8, ['3', 3])
+#     [5] ('1.8 mA', 16, ['4', 4])
+#     [6] ('180 µA', 32, ['5', 5])
+#     [7] ('18 µA', 64, ['6', 6])
+#     [8] ('off', 0, [])
+
+# Parameter: v_range
+#   Options:
+#     [0] ('15V', 0, ['low', 0])
+#     [1] ('5V', 1, ['high', 1])
+
+# Parameter: ovr_to_lsb
+#   Options:
+#     [0] ('off', 0, [])
+#     [1] ('on', 1, [])
+
+# Parameter: trigger_source
+#   Options:
+#     [0] ('auto', 0, [])
+#     [1] ('gpi0', 2, [])
+#     [2] ('gpi1', 3, [])
+
+# Parameter: io_voltage
+#   Options:
+#     [0] ('1.8V', 1800, [])
+#     [1] ('2.1V', 2100, [])
+#     [2] ('2.5V', 2500, [])
+#     [3] ('2.7V', 2700, [])
+#     [4] ('3.0V', 3000, [])
+#     [5] ('3.3V', 3300, [])
+#     [6] ('3.6V', 3600, [])
+#     [7] ('5.0V', 5000, [])
+
+# Parameter: gpo0
+#   Options:
+#     [0] ('0', 0, [0])
+#     [1] ('1', 1, [1])
+
+# Parameter: gpo1
+#   Options:
+#     [0] ('0', 0, [0])
+#     [1] ('1', 1, [1])
+
+# Parameter: current_lsb
+#   Options:
+#     [0] ('normal', 0, [])
+#     [1] ('gpi0', 2, [])
+#     [2] ('gpi1', 3, [])
+
+# Parameter: voltage_lsb
+#   Options:
+#     [0] ('normal', 0, [])
+#     [1] ('gpi0', 2, [])
+#     [2] ('gpi1', 3, [])
+
+# Parameter: control_test_mode
+#   Options:
+#     [0] ('normal', 3, [])
+#     [1] ('usb', 129, [])
+#     [2] ('fpga', 130, [])
+#     [3] ('both', 131, [])
+
+# Parameter: transfer_length
+#   Options:
+#     [0] ('1', 1, [])
+#     [1] ('2', 2, [])
+#     [2] ('4', 4, [])
+#     [3] ('8', 8, [])
+#     [4] ('16', 16, [])
+#     [5] ('32', 32, [])
+#     [6] ('64', 64, [])
+#     [7] ('128', 128, [])
+#     [8] ('256', 256, [])
+
+# Parameter: transfer_outstanding
+#   Options:
+#     [0] ('1', 1, [])
+#     [1] ('2', 2, [])
+#     [2] ('4', 4, [])
+#     [3] ('8', 8, [])
+
+# Parameter: current_ranging
+#   Current Value: interp_1_n_1
+
+# Parameter: current_ranging_type
+#   Options:
+#     [0] ('off', 'off', [])
+#     [1] ('mean', 'mean', [])
+#     [2] ('interp', 'interp', ['interpolate'])
+#     [3] ('NaN', 'nan', ['nan'])
+
+# Parameter: current_ranging_samples_pre
+#   Options:
+#     [0] ('0', 0, [0])
+#     [1] ('1', 1, [1])
+#     [2] ('2', 2, [2])
+#     [3] ('3', 3, [3])
+#     [4] ('4', 4, [4])
+#     [5] ('5', 5, [5])
+#     [6] ('6', 6, [6])
+#     [7] ('7', 7, [7])
+#     [8] ('8', 8, [8])
+
+# Parameter: current_ranging_samples_window
+#   Options:
+#     [0] ('m', 'm', [])
+#     [1] ('n', 'n', [])
+#     [2] ('0', 0, [0])
+#     [3] ('1', 1, [1])
+#     [4] ('2', 2, [2])
+#     [5] ('3', 3, [3])
+#     [6] ('4', 4, [4])
+#     [7] ('5', 5, [5])
+#     [8] ('6', 6, [6])
+#     [9] ('7', 7, [7])
+#     [10] ('8', 8, [8])
+#     [11] ('9', 9, [9])
+#     [12] ('10', 10, [10])
+#     [13] ('11', 11, [11])
+#     [14] ('12', 12, [12])
+
+# Parameter: current_ranging_samples_post
+#   Options:
+#     [0] ('0', 0, [0])
+#     [1] ('1', 1, [1])
+#     [2] ('2', 2, [2])
+#     [3] ('3', 3, [3])
+#     [4] ('4', 4, [4])
+#     [5] ('5', 5, [5])
+#     [6] ('6', 6, [6])
+#     [7] ('7', 7, [7])
+#     [8] ('8', 8, [8])
+
+# Parameter: buffer_duration
+#   Options:
+#     [0] ('15 seconds', 15, [15])
+#     [1] ('30 seconds', 30, [30])
+#     [2] ('1 minute', 60, [60])
+#     [3] ('2 minutes', 120, [120])
+#     [4] ('5 minutes', 300, [300])
+#     [5] ('10 minutes', 600, [600])
+#     [6] ('20 minutes', 1200, [1200])
+#     [7] ('1 hour', 3600, [3600])
+#     [8] ('2 hours', 7200, [7200])
+#     [9] ('5 hours', 18000, [18000])
+#     [10] ('10 hours', 36000, [36000])
+#     [11] ('1 day', 86400, [86400])
+
+# Parameter: reduction_frequency
+#   Options:
+#     [0] ('100 Hz', 100, [100])
+#     [1] ('50 Hz', 50, [50])
+#     [2] ('20 Hz', 20, [20])
+#     [3] ('10 Hz', 10, [10])
+#     [4] ('5 Hz', 5, [5])
+#     [5] ('2 Hz', 2, [2])
+#     [6] ('1 Hz', 1, [1])
+
+# Parameter: sampling_frequency
+#   Options:
+#     [0] ('2 MHz', 2000000, [2000000, 'auto', None, 'default'])
+#     [1] ('1 MHz', 1000000, [1000000])
+#     [2] ('500 kHz', 500000, [500000])
+#     [3] ('200 kHz', 200000, [200000])
+#     [4] ('100 kHz', 100000, [100000])
+#     [5] ('50 kHz', 50000, [50000])
+#     [6] ('20 kHz', 20000, [20000])
+#     [7] ('10 kHz', 10000, [10000])
+#     [8] ('5 kHz', 5000, [5000])
+#     [9] ('2 kHz', 2000, [2000])
+#     [10] ('1 kHz', 1000, [1000])
+#     [11] ('500 Hz', 500, [500])
+#     [12] ('200 Hz', 200, [200])
+#     [13] ('100 Hz', 100, [100])
+#     [14] ('50 Hz', 50, [50])
+#     [15] ('20 Hz', 20, [20])
+#     [16] ('10 Hz', 10, [10])
+"""
